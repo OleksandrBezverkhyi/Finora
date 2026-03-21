77b462d7f454d4927824f4090a8350cb1ab09bd7
OleksandrBezverkhyi <bezverkhyi.ol@gmail.com>
2026-03-21 19:41:37 +0100

    chore: add prettier and project config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,18 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  {
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "node_modules/**",
     "next-env.d.ts",
   ]),
 ]);
